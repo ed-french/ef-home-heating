044935cdc7003bdfc709ef4650b506e5a7bd9c5c
ed french <edwardmfrench@gmail.com>
2018-03-21 19:12:12 +0000

    Testing retreiving settings

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import webapp2
 
+from google.cloud import datastore
+
+project_id="ef-home-heating"
+
+def create_client(project_id):
+    return datastore.Client(project_id)
+
+def get_settings(project_id):
+    client=create_client(project_id)
+    query = client.query(kind='settings')
+    query.order = ['created']
+    return list(query.fetch())
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+        self.response.write(get_settings(project_id))
 
 
 app = webapp2.WSGIApplication([
